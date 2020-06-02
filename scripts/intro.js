b111b2c97b38558c23a9ef8fc4f64b82d5c5a8ab
Higor <higorlinosilva1@gmail.com>
2020-06-02 16:36:45 -0300

    Adicionado página de cadastro

---
+++ scripts/intro.js
@@ -0,0 +1,63 @@
+//comentários
+//document.write("hello");
+//variáveis e tipo de dados
+
+/*var myvar = "Hello";
+const myconst = "He";
+document.write(myvar);*/
+
+/*const s1 ="Isso é uma string"
+const s2 = 'Também é uma string'
+const s3 = ´tambem é uma string´;*/
+
+//number
+/*const n1 = 1;
+const n2 = 12;
+document.write(n1+n2);*/
+
+//boolean
+/*const btrue = true
+const bfalse = false
+document.write(bfalse);*/
+
+//OBJETOS
+  /*
+const pessoa ={
+  altura:"1,80m",
+  idade: 24,
+  solteiro: true,
+  correr (){
+    return("run Forest")
+  }
+}
+document.write(pessoa.correr())
+*/
+
+//Array
+/*const colecaoDeBolinhas = ["blue", "green", 1]
+document.write(colecaoDeBolinhas[0])*/
+
+//funções
+/*
+function sayMyName(){
+  document.write("Mauricio");
+}
+sayMyName();
+*/
+//parametros
+/*function sayMyName(name){
+  document.write(name);
+}
+sayMyName("Douglas")*/
+
+/*const notaFinal = 4
+if (notaFinal > 5){
+  document.write("Aprovado")
+}else{
+  document.write("Reprovado")
+}*/
+
+//loop repetições
+//for (i = 0; i<10; i++){
+//    document.write(`<p>${i}</p>`);
+//  }
